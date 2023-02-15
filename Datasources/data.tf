47b7e9d124644519924f143818978b401a265e02
sowmyaraj <sowmyaraj435@gmail.com>
2023-02-15 20:00:05 +0530

    terraform output and datasourses

---
+++ Datasources/data.tf
@@ -0,0 +1,26 @@
+
+data "aws_ami" "example" {
+  
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+output "ami"{
+     
+ value = data.aws_ami.example    
+}
+
+data "aws_instance" "foo" {
+  instance_id = "i-0bfa88ba77cb6582c"
+}
+
+output "instance" {
+
+    value = data.aws_instance.foo.public_ip
+}
+
+output "dns" {
+
+    value = data.aws_instance.foo.public_dns
+}
